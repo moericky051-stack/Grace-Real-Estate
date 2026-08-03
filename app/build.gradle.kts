606c0ca2e847d1be7911229be7f2e91905b35a6c
moericky051-stack <moericky051@gmail.com>
2026-08-03 13:11:07 +0630

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,12 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    // Version Incompatibility ကို ကျော်ပြီး ဆောက်ခိုင်းသည့် Setting ထည့်ပေးထားပါသည်
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf(
+            "-Xskip-metadata-version-check"
+        )
     }
 
     buildFeatures {
