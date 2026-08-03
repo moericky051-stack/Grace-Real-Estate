58cde24648d3b78c5964e6d0d9a800cffb204838
moericky051-stack <moericky051@gmail.com>
2026-08-03 10:52:46 +0630

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    
-    // KSP Plugin Version (Kotlin Version 1.9.22 နဲ့ ကိုက်ညီအောင် ညှိထားပါသည်)
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
-    
-    // Google Services (Firebase သုံးထားပါက)
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.roborazzi) apply false
+    alias(libs.plugins.secrets) apply false
 }
 
 tasks.register("clean", Delete::class) {
